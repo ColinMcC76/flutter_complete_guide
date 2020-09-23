ae8c9f7ee54831325cb1ee2518bc62809a49951e
Colin McCulloch <colinmucculloch@gmail.com>
2020-09-23 10:06:03 -0400

    chnged background color to red

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,67 @@
 import 'package:flutter/material.dart';
+
 void main() => runApp(MyApp());
 
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
 
-class MyApp extends StatelessWidget {
-  Widget build(BuildContext context){
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text('Complete Flutter Guide'),),
-      body: Text('This is my default text!'),
-    ),);
+class MyAppState extends State<MyApp> {
+  int questionIndex = 0;
+  void answerQuestion() {
+    questionIndex += 1;
+  }
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('Main body'),
+      ),
+    );
   }
 }
+// class MyApp extends StatefulWidget {
+//   var questionIndex = 0;
+
+//   void answerQuestion() {
+//     questionIndex = questionIndex + 1;
+
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var questions = [
+//       'What\'s your favoitre color?',
+//       'What\'s your favoite animal?',
+//     ];
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Complete Flutter Guide'),
+//         ),
+//         body: Column(
+//           children: [
+//             Text(questions[questionIndex]),
+//             RaisedButton(
+//               child: Text('Answer 1'),
+//               onPressed: answerQuestion,
+//             ),
+//             RaisedButton(
+//               child: Text('Answer 2'),
+//               onPressed: () => {print('Answer 2 confirmed')},
+//             ),
+//             RaisedButton(
+//               child: Text('Answer 3'),
+//               onPressed: () => {print('Answer 3 confirmed')},
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
